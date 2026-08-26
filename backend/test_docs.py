e7f1bc360115a7fb9ec7c6c6f6a83a2bc5578134
Prince Osei Bonsu <Posei231846@voorhees.edu>
2026-08-26 17:06:59 -0400

    add news pagination, tag filter, single article endpoint, news search, clean duplicate articles

---
+++ backend/test_docs.py
@@ -0,0 +1,14 @@
+﻿import requests
+
+r = requests.post("http://127.0.0.1:8000/login", json={
+    "email": "prince@arriv0test.com",
+    "password": "Test1234!"
+})
+token = r.json().get("access_token")
+user_id = r.json().get("user_id")
+print("User ID: " + str(user_id))
+
+r2 = requests.get("http://127.0.0.1:8000/documents",
+    headers={"authorization": "Bearer " + token}
+)
+print(r2.json())
